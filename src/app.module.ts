50c1f51b6b4f925d0cddc9cd2e2e3dd1d3cd259c
peter_vida <peter_vida1@epam.com>
2024-01-23 11:08:27 +0100

    dotenv config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MnemoController } from './mnemo/mnemo.controller';
 import { MnemoService } from './mnemo/mnemo.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController, MnemoController],
   providers: [AppService, MnemoService],
 })
